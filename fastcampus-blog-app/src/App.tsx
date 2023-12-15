f9c94a06d74386500e64395e37cb36d3b06de00d
songye38 <ddev0308@gmail.com>
2023-12-16 00:28:48 +0900

    ADD router

--- fastcampus-blog-app/src/App.tsx
+++ fastcampus-blog-app/src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route,Routes,Navigate,Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/posts'>Post Link</Link></li>
+        <li><Link to='/posts/:id'>Post Detail List</Link></li>
+        <li><Link to='/posts/new'>Post New Page</Link></li>
+        <li><Link to='/posts/edit/:id'>Post edit page</Link></li>
+        <li><Link to='/profile'>Profile</Link></li>
+      </ul>
     </div>
+    <Routes>
+      <Route path='/' element={<h1>Hoem Page</h1>} />
+      <Route path='/posts' element={<h1>Hoem Page</h1>} />
+      <Route path='/posts/:id' element={<h1>post detail page</h1>} />
+      <Route path='/posts/new' element={<h1>add new page</h1>} />
+      <Route path='/posts/edit/:id' element={<h1>Hoem Detail Page</h1>} />
+      <Route path='/profile' element={<h1>profile page</h1>} />
+      <Route path='*' element={<Navigate replace to='/'/>} />
+    </Routes>
+    </>
   );
 }
 

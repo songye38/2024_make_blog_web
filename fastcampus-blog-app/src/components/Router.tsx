7682c3aa7f1ecee2994b0217c85a47edb06a236f
songye38 <ddev0308@gmail.com>
2023-12-17 20:30:25 +0900

    UPDATE router.tsx 인증이 안된 사용자에게 로그인 , 회원가입 페이지만 보이도록

--- fastcampus-blog-app/src/components/Router.tsx
+++ fastcampus-blog-app/src/components/Router.tsx
@@ -1,4 +1,5 @@
 import {Route,Routes,Navigate} from 'react-router-dom'
+import { useState } from 'react';
 import Home from 'pages/home';
 import PostList from 'pages/posts';
 import PostDetail from 'pages/posts/detail';
@@ -9,18 +10,31 @@ import LoginPage from 'pages/login';
 import SignupPage from 'pages/signup';
 
 export default function Router() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   return (
     <>
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/posts' element={<PostList />} />
-      <Route path='/posts/:id' element={<PostDetail />} />
-      <Route path='/posts/new' element={<PostNew />} />
-      <Route path='/posts/edit/:id' element={<PostEdit />} />
-      <Route path='/profile' element={<ProfilePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignupPage/>} />
-      <Route path='*' element={<Navigate replace to='/'/>} />
+      {isAuthenticated ? (
+        <>
+          <Route path='/' element={<Home />} />
+          <Route path='/posts' element={<PostList />} />
+          <Route path='/posts/:id' element={<PostDetail />} />
+          <Route path='/posts/new' element={<PostNew />} />
+          <Route path='/posts/edit/:id' element={<PostEdit />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage/>} />
+          <Route path='*' element={<Navigate replace to='/'/>} />
+        </>
+      ) : (
+        <>
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/signup' element={<SignupPage/>} />
+          <Route path='*' element={<LoginPage/>} />
+        </>
+      )}
+      
     </Routes>
     </>
   );

b75779ebc43606615ea83d721b41a50668a1823f
songye38 <ddev0308@gmail.com>
2023-12-17 18:07:25 +0900

    UPDATE relative path

--- fastcampus-blog-app/src/pages/home/index.tsx
+++ fastcampus-blog-app/src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom'
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import PostList from '../../components/PostList';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import PostList from 'components/PostList';
 
 export default function Home(){
     return (

438370043d35c4baf50390c53ed7bab64e84da35
songye38 <ddev0308@gmail.com>
2023-12-18 00:53:44 +0900

    UPDATE App.tsx 원래 내 코드에서 ; 추가

--- fastcampus-blog-app/src/App.tsx
+++ fastcampus-blog-app/src/App.tsx
@@ -1,38 +1,40 @@
 import { useEffect, useState } from "react";
 import { app } from "firebaseApp";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {getAuth,onAuthStateChanged} from 'firebase/auth';
 
 import Router from "./components/Router";
 import Loader from "components/Loader";
 
+
 function App() {
   const auth = getAuth(app);
-  // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도
-  const [init, setInit] = useState<boolean>(false);
-  // auth의 currentUser가 있으면 authenticated로 변경
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
-    !!auth?.currentUser
-  );
+  //auth를 체크하기 전에는 loader를 띄어주는 용도
+  const [init,setInit] = useState<boolean>(false);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
+  //auth의 current user가 있으면 autehnticated로 바뀌는것.
+  const [isAuthenticated,setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
         setIsAuthenticated(true);
-      } else {
+      }else{
         setIsAuthenticated(false);
       }
       setInit(true);
     });
-  }, [auth]);
-
+  },[auth]);
   return (
     <>
       <ToastContainer />
-      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
+      {init ? <Router isAuthenticated = {isAuthenticated} /> : <Loader />}
     </>
+
   );
+  
+
 }
 
 export default App;

1f06c7c220020560994bc9a0213be6288f24ce2c
songye38 <ddev0308@gmail.com>
2023-12-17 22:34:02 +0900

    UPDATE SignupForm.tsx firebase와 signupform 연동

--- fastcampus-blog-app/src/components/SignupForm.tsx
+++ fastcampus-blog-app/src/components/SignupForm.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import {app} from 'firebaseApp';
+import {getAuth,createUserWithEmailAndPassword} from 'firebase/auth';
 
 export default function SignupForm(){
 
@@ -7,6 +9,16 @@ export default function SignupForm(){
     const [email,setEmail] = useState<string>("");
     const [password,setPassword] = useState<string>("");
     const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+
+    const onSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        try {
+            const auth = getAuth(app);
+            await createUserWithEmailAndPassword(auth,email,password);
+        }catch(error){
+            console.log(error);
+        }
+    }
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {
@@ -51,7 +63,7 @@ export default function SignupForm(){
     };
 
     return (
-        <form action='/post' method='POST' className="form form-lg">
+        <form onSubmit={onSubmit} method='POST' className="form form-lg">
             <h1 className="form__title">회원가입</h1>
             <div className="form__block">
             <label htmlFor="email">이메일</label>

66953f7b79aab5baf5f508c51bd8b35125c1ae8f
songye38 <ddev0308@gmail.com>
2023-12-20 23:37:55 +0900

    Update Footer.tsx div로 link 요소들만 감싸주기

--- fastcampus-blog-app/src/components/Footer.tsx
+++ fastcampus-blog-app/src/components/Footer.tsx
@@ -7,9 +7,11 @@ export default function Footer(){
     const context = useContext(ThemeContext)
     return (
         <footer>
+            <div>
                 <Link to="/posts/new">글쓰기</Link>
                 <Link to="/posts">게시글</Link>
                 <Link to="/profile">프로필</Link>
+            </div>
             <div>
                 {context.theme ==='light' ? <BsSun /> : <BsMoonFill />}
             </div>

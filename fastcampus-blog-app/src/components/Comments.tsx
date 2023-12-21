658eaaa4ccdf84dbdc85765f9819207eaa4eadcf
songye38 <ddev0308@gmail.com>
2023-12-22 01:41:17 +0900

    Update Comments.tsx 목업 댓글을 만들어 list로 보여주기

--- fastcampus-blog-app/src/components/Comments.tsx
+++ fastcampus-blog-app/src/components/Comments.tsx
@@ -1,4 +1,44 @@
 import { useState } from "react"
+
+
+const COMMENTS = [
+    {
+        id : 1,
+        email : "vsongyev@hanmail.net",
+        content : '댓글입니다 1',
+        createdAt : '2023-12-22',
+    },
+    {
+        id : 2,
+        email : "vsongyev@hanmail.net",
+        content : '댓글입니다 2',
+        createdAt : '2023-12-22',
+    },
+    {
+        id : 3,
+        email : "vsongyev@hanmail.net",
+        content : '댓글입니다 3',
+        createdAt : '2023-12-22',
+    },
+    {
+        id : 4,
+        email : "vsongyev@hanmail.net",
+        content : '댓글입니다 4',
+        createdAt : '2023-12-22',
+    },
+    {
+        id : 5,
+        email : "vsongyev@hanmail.net",
+        content : '댓글입니다 5',
+        createdAt : '2023-12-22',
+    },
+]
+
+
+
+
+
+
 export default function Comments(){
     const [comment,setComment] = useState("")
     const onChange =(e : React.ChangeEvent<HTMLTextAreaElement>)=>{
@@ -21,7 +61,18 @@ export default function Comments(){
                 <input type='submit' value='입력' className="form__btn-submit"/>
             </div>
         </form>
-        <div className="comments__list"></div>
+        <div className="comments__list">
+            {COMMENTS?.map((comment)=>(
+                <div key={comment.id} className="comment__box">
+                    <div className="comment__profile-box">
+                        <div className="comment__email">{comment?.email}</div>
+                        <div className="comment__date">{comment?.createdAt}</div>
+                        <div className="comment__delete">삭제</div>
+                    </div>
+                    <div className="comment__text">{comment?.content}</div>
+                </div>
+            ))}
+        </div>
     </div>
     )
     

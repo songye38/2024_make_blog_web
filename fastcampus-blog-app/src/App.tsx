27760103745fc91f78eb7664808aa723e5e0b72a
songye38 <ddev0308@gmail.com>
2023-12-22 01:09:31 +0900

    Update App.tsx 메인 컴포넌트를 div로 감싸고 className 추가

--- fastcampus-blog-app/src/App.tsx
+++ fastcampus-blog-app/src/App.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState ,useContext} from "react";
 import { app } from "firebaseApp";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {getAuth,onAuthStateChanged} from 'firebase/auth';
+import ThemeContext from "context/ThemeContext";
 
 import Router from "./components/Router";
 import Loader from "components/Loader";
 
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   //auth를 체크하기 전에는 loader를 띄어주는 용도
   const [init,setInit] = useState<boolean>(false);
@@ -27,14 +29,10 @@ function App() {
     });
   },[auth]);
   return (
-    <>
+    <div className={context.theme==='light' ? 'white'  : 'dark'}>
       <ToastContainer />
       {init ? <Router isAuthenticated = {isAuthenticated} /> : <Loader />}
-    </>
-
+    </div>
   );
-  
-
 }
-
 export default App;
